f346eaf2dec29ee633910bd402a38e95cc5a3c2b
Lonnie Hansen <lg3hansen@gmail.com>
2017-09-01 17:55:15 -0700

    Added compiler.cpp, compiler.h and main.cpp

---
+++ Compiler/compiler.h
@@ -0,0 +1,13 @@
+#ifndef compiler_h
+#define compiler_h
+#include <string>
+
+std::string gettoken() {
+
+}
+
+std::string getlexeme() {
+
+}
+
+#endif // !compiler_h
---
+++ Compiler/main.cpp
@@ -0,0 +1,5 @@
+#include "compiler.h"
+
+int main() {
+
+}
