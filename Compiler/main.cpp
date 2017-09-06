7a83965233def236ec05fd3be195b1ccc5754b61
Lonnie Hansen <lg3hansen@gmail.com>
2017-09-05 22:04:10 -0700

    Got the very basic lexer working.

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -1,10 +1,11 @@
 #include "compiler.h"
 #include <iostream>
 #include <fstream>
+#include <assert.h>
 
 int main() {
 	std::ifstream input;
-	input.open("test.txt");
+	input.open("C:\\Users\\Lonnie\\Source\\Repos\\Compiler\\Compiler\\test.txt");
 	std::string token;
 	std::string lexeme;
 	token = gettoken(input);
