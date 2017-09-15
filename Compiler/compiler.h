900655a3c2f4a27fee257397ae7847dc7a314e72
Lonnie Hansen <lg3hansen@gmail.com>
2017-09-14 23:43:41 -0700

    Defined functions in compiler.h

--- Compiler/compiler.h
+++ Compiler/compiler.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef compiler_h
 #define compiler_h
 
@@ -9,13 +8,17 @@
 #include <algorithm>
 #include <iomanip>
 
-//std::string keywords[] = {"while", "if", "int", "fi", "else", "return", "read", "write"};
-
+/*Lexical Analyzer function that currently just prints tokens/lexemes*/
 void lexer(std::ifstream& input);
+/*Returns whether to go to ID, int/real, or the other states*/
 int getstate(char c);
+/*Returns whether a number, period, or other character is found and goes to that state*/
 int numstate(char c);
+/*Returns whether a letter, '#', or number is found and goes to that state*/
 int idstate(char c);
+/*Returns true if character value is a separator as defined in vector*/
 bool isseparator(char c);
+/*Returns true if character value is a operator as defined in vector*/
 bool isoperator(char c);
 
 #endif // !compiler_h
