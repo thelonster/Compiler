ae82c39eeaafbb4c821edac7f19c65bfb09e58dd
Lonnie Hansen <lg3hansen@gmail.com>
2017-09-18 13:53:48 -0700

    Changed all tabs to spaces so the code looks nicer on github.

--- Compiler/compiler.cpp
+++ Compiler/compiler.cpp
@@ -2,25 +2,25 @@
 #include "macros.h"
 
 void lexer(std::ifstream& input) {
-	std::string token;
-	char c;
-	bool floatflag = false, tokenfound = false, hashflag = false, keyflag = false;
-	while (input.get(c)) {
-		tokenfound = false;
-		switch (getstate(c)) {
-			//This case is for ID's
-			case ID_STATE:
-				//state 1 -> 2: adding letter then moving to second state
-				token += c;
-				while (!tokenfound && input.get(c)) {
+    std::string token;
+    char c;
+    bool floatflag = false, tokenfound = false, hashflag = false, keyflag = false;
+    while (input.get(c)) {
+        tokenfound = false;
+        switch (getstate(c)) {
+            //This case is for ID's
+            case ID_STATE:
+                //state 1 -> 2: adding letter then moving to second state
+                token += c;
+                while (!tokenfound && input.get(c)) {
                     //This switch is the 2nd state that can move to the 3rd/4th/5th/6th state
-					switch (idstate(c)) {
+                    switch (idstate(c)) {
                         //letter state
-						case ID_LETTER:
-						    token += c;
+	                    case ID_LETTER:
+	                        token += c;
                             hashflag = false; //if a number is added, reset hashflag
-						    //break moves from state 3/6 to state 2
-						    break;
+	                        //break moves from state 3/6 to state 2
+	                        break;
                         //# state
                         case ID_POUND:
                             //Can't have 2 # signs in a row
@@ -52,37 +52,37 @@ void lexer(std::ifstream& input) {
                             keyflag = false;
                             token = "";
                             break;
-					}
-				}
-				break;
-			//This state is for ints and floats
+                    }
+                }
+                break;
+            //This state is for ints and floats
 			case NUMBER_STATE:
-				do {
+                do {
                     //This switch is the 1st state where is can move to the 2nd/3rd/4th state
-					switch (numstate(c))	{
-						//number state
-						case NUM_NUMBER:
-							token += c;
-							break;
-						//period state
-						case NUM_PERIOD:
-							floatflag = true;
-							token += c;
-							break;
-						//accepting state
-						default:
-							input.unget();
-							if (floatflag)
-								std::cout << "float\t\t" << token << std::endl;
-							else
-								std::cout << "int\t\t" << token << std::endl;
-							floatflag = false;
-							token = "";
-							tokenfound = true;
-							break;
-					}
-				} while (!tokenfound && input.get(c));
-				break;
+                    switch (numstate(c))	{
+                        //number state
+                        case NUM_NUMBER:
+                            token += c;
+                            break;
+                        //period state
+                        case NUM_PERIOD:
+                            floatflag = true;
+                            token += c;
+                            break;
+                        //accepting state
+                        default:
+                            input.unget();
+                            if (floatflag)
+                                std::cout << "float\t\t" << token << std::endl;
+                            else
+                                std::cout << "int\t\t" << token << std::endl;
+                            floatflag = false;
+                            token = "";
+                            tokenfound = true;
+                            break;
+                    }
+                } while (!tokenfound && input.get(c));
+                break;
             //state for operators & separators (also blank spaces)
             default:
                 if (isseparator(c)) {
@@ -106,51 +106,51 @@ void lexer(std::ifstream& input) {
                     token = "";
                 }
                 break;
-		}
-	}
+        }
+    }
 }
 
 int getstate(char c) {
-	if (isalpha(c))
-		return ID_STATE;
-	else if (isdigit(c))
-		return NUMBER_STATE;
-	else
-		return -1;
+    if (isalpha(c))
+        return ID_STATE;
+    else if (isdigit(c))
+        return NUMBER_STATE;
+    else
+        return -1;
 }
 
 int numstate(char c) {
-	if (isdigit(c))
-		return NUM_NUMBER;
-	else if (c == '.')
-		return NUM_PERIOD;
-	else
-		return -1;
+    if (isdigit(c))
+        return NUM_NUMBER;
+    else if (c == '.')
+        return NUM_PERIOD;
+    else
+        return -1;
 }
 
 int idstate(char c) {
-	if (isalpha(c))
-		return ID_LETTER;
-	else if (c == '#')
-		return ID_POUND;
-	else if (isdigit(c))
-		return ID_NUMBER;
-	else
-		return -1;
+    if (isalpha(c))
+        return ID_LETTER;
+    else if (c == '#')
+        return ID_POUND;
+    else if (isdigit(c))
+        return ID_NUMBER;
+    else
+        return -1;
 }
 
 bool isseparator(char c) {
-	for (auto it = separators.begin(); it != separators.end(); it++)
-		if (*it == c)
-			return true;
-	return false;
+    for (auto it = separators.begin(); it != separators.end(); it++)
+        if (*it == c)
+            return true;
+    return false;
 }
 
 bool isoperator(char c) {
-	for (auto it = operators.begin(); it != operators.end(); it++)
-		if (*it == c)
-			return true;
-	return false;
+    for (auto it = operators.begin(); it != operators.end(); it++)
+        if (*it == c)
+            return true;
+    return false;
 }
 
 int main() {
